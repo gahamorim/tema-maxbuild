ffdd6064605608e3342581423555d366ca575b49
Gabriel Amorim <gaahamorim95@gmail.com>
2020-01-27 18:29:37 -0300

    Top Header

--- index.php
+++ index.php
@@ -1,8 +1,21 @@
 <html>
     <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+        <script src="https://kit.fontawesome.com/a5ddd9268e.js" crossorigin="anonymous"></script>
+        <link rel="stylesheet" type="text/css" href="styles.css">
         <title>MaxBuild</title>
     </head>
     <body>
-        <h1>Teste</h1>
+        <div class="top-header-conteiner">
+            <div class="grid-infos-conteiner">
+                <div class="grid-infos-item"> <i class="fas fa-phone-alt" style="color: white;"></i> Call: (719)445-2808</div>
+                <div class="grid-infos-item"> <i class="far fa-clock" style="color: white;"></i> 24/7 Emergency Service</div>
+                <div class="grid-infos-item"> <i class="fas fa-home" style="color: white;"></i> 400 Madison Street, VA, US</div>
+                <div class="grid-infos-item"> </div>
+                <div class="grid-infos-item"> Login</div>
+            </div>
+        </div>
     </body>
 </html>
